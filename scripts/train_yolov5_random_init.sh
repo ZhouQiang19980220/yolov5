062d324766dad3082f426ddac9a81d2365177996
zhou_qiang98 <zhou_qiang98@163.com>
2023-10-09 23:04:45 +0800

    训练脚本

--- scripts/train_yolov5_random_init.sh
+++ scripts/train_yolov5_random_init.sh
@@ -1,1 +1,3 @@
 python train.py --img 640 --epochs 300 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s.yaml
+
+python train.py --img 640 --epochs 300 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml
