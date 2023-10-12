b31eb6cab9a222a0813452ab2ea167512c94e823
zhou_qiang98 <zhou_qiang98@163.com>
2023-10-12 10:43:20 +0800

    更新训练脚本

--- scripts/train_yolov5_random_init.sh
+++ scripts/train_yolov5_random_init.sh
@@ -1,3 +1,19 @@
-python train.py --img 640 --epochs 300 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s.yaml
+# python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s.yaml
 
-python train.py --img 640 --epochs 300 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s.yaml --project runs/train/yolov5_baseline_随机初始化
+
+# python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv_transformer_neck.yaml
+
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --hyp data/hyps/hyp.scratch-low_label_smooth.yaml
+
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --hyp data/hyps/hyp.scratch-low_label_smooth=0.1.yaml
+
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --hyp data/hyps/hyp.scratch-low_label_smooth=0.3.yaml
+
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --hyp data/hyps/hyp.scratch-low_label_smooth=0.7.yaml
+
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --hyp data/hyps/hyp.scratch-low_label_smooth=0.9.yaml
+
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv_average_trainsformer_neck.yaml
+
+yolo train data=SARAIRcraft1.0.yaml model=yolov8s.pt epochs=200 lr0=0.01
