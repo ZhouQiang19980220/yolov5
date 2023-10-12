41e70594b27003f9ab652fda487f21c3ae9662b7
zhou_qiang98 <zhou_qiang98@163.com>
2023-10-12 18:23:19 +0800

    更新训练脚本

--- scripts/train_yolov5_random_init.sh
+++ scripts/train_yolov5_random_init.sh
@@ -2,9 +2,9 @@
 
 python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s.yaml --project runs/train/yolov5_baseline_随机初始化
 
-# python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv_transformer_neck.yaml
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv_transformer_neck.yaml 
 
-python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --hyp data/hyps/hyp.scratch-low_label_smooth.yaml
+python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --project runs/train/yolov5_sparse_conv
 
 python train.py --img 640 --epochs 200 --data SARAIRcraft1.0.yaml --weights '' --cache ram --cfg yolov5s_sparse_conv.yaml --hyp data/hyps/hyp.scratch-low_label_smooth=0.1.yaml
 
